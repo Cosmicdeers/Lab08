b46c67b1e572944da1e335190dcf66307ea49bee
mbalint <mbalint@ualberta.ca>
2025-10-29 19:22:43 -0700

    "Added failing add test"

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,36 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    private CustomList mockCityList() {
+        CustomList cityList = new CustomList();
+        cityList.add(mockCity());
+        return cityList;
+    }
+
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
+    @Test
+    void testAdd() {
+        CustomList cityList = mockCityList();
+        assertEquals(1, cityList.getCities().size());
+        City city = new City("Regina", "Saskatchewan");
+        cityList.add(city);
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains(city));
+    }
+
+    @Test
+    void testAddException() {
+        CustomList cityList = mockCityList();
+        City city = new City("Yellowknife", "Northwest Territories");
+        cityList.add(city);
+        assertThrows(IllegalArgumentException.class, () -> {
+            cityList.add(city);
+        });
+    }
+
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
